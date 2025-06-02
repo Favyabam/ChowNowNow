bf56653e3b75b74d12aa1b6a3845e0e44381e4f6
Favyabam <favourdickens82@yahoo.com>
2025-06-02 08:49:22 +0100

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,308 @@
+import basket_icon from "./basket_icon.png";
+import logo from "./logo.png";
+import header from "./header.png";
+import search_icon from "./search_icon.png";
+import menu_1 from "./menu_1.png";
+import menu_2 from "./menu_2.png";
+import menu_3 from "./menu_3.png";
+import menu_4 from "./menu_4.png";
+import menu_5 from "./menu_5.png";
+import menu_6 from "./menu_6.png";
+import menu_7 from "./menu_7.png";
+
+import food_1 from "./food_1.png";
+import food_2 from "./food_2.png";
+import food_3 from "./food_3.png";
+import food_4 from "./food_4.png";
+import food_5 from "./food_5.png";
+import food_6 from "./food_6.png";
+import food_7 from "./food_7.png";
+import food_8 from "./food_8.png";
+import food_9 from "./food_9.png";
+import food_10 from "./food_10.png";
+import food_11 from "./food_11.png";
+import food_12 from "./food_12.png";
+import food_13 from "./food_13.png";
+import food_14 from "./food_14.png";
+import food_15 from "./food_15.png";
+import food_16 from "./food_16.png";
+import food_17 from "./food_17.png";
+import food_18 from "./food_18.png";
+import food_19 from "./food_19.png";
+import food_20 from "./food_20.png";
+import food_21 from "./food_21.png";
+import food_22 from "./food_22.png";
+import food_23 from "./food_23.png";
+import food_24 from "./food_24.png";
+import food_25 from "./food_25.png";
+import food_26 from "./food_26.png";
+import food_27 from "./food_27.png";
+import food_28 from "./food_28.png";
+
+import add_icon_white from "./add_icon_white.png";
+import add_icon_green from "./add_icon_green.png";
+import remove_icon_red from "./remove_icon_red.png";
+import app_store from "./app_store.png";
+import play_store from "./play_store.png";
+import linkedin_icon from "./linkedin_icon.png";
+import facebook_icon from "./facebook_icon.png";
+import twitter_icon from "./twitter_icon.png";
+import cross_icon from "./cross_icon.png";
+import selector_icon from "./selector_icon.png";
+import rating_starts from "./rating_starts.png";
+import profile_icon from "./profile_icon.png";
+import bag_icon from "./bag_icon.png";
+import logout_icon from "./logout_icon.png";
+import parcel_icon from "./parcel_icon.png";
+
+export const assets = {
+  logo,
+  basket_icon,
+  header,
+  search_icon,
+  rating_starts,
+  add_icon_green,
+  add_icon_white,
+  remove_icon_red,
+  app_store,
+  play_store,
+  linkedin_icon,
+  facebook_icon,
+  twitter_icon,
+  cross_icon,
+  selector_icon,
+  profile_icon,
+  logout_icon,
+  bag_icon,
+  parcel_icon,
+};
+
+export const menu_list = [
+  {
+    menu_name: "Burgers",
+    menu_image: menu_1,
+  },
+  {
+    menu_name: "Snacks & Small Chops",
+    menu_image: menu_2,
+  },
+  {
+    menu_name: "Shawarma & Wraps",
+    menu_image: menu_3,
+  },
+  {
+    menu_name: "Sides & Extras",
+    menu_image: menu_4,
+  },
+  {
+    menu_name: "Fried Chicken",
+    menu_image: menu_5,
+  },
+  {
+    menu_name: "Nigerian Meals",
+    menu_image: menu_6,
+  },
+  {
+    menu_name: "Drinks & Desserts",
+    menu_image: menu_7,
+  },
+];
+
+export const food_list = [
+  {
+    _id: "1",
+    name: "Beef Burger with Egg",
+    image: food_1,
+    price: 15000,
+    category: "Burgers",
+  },
+  {
+    _id: "2",
+    name: "Chicken Suya Burger",
+    image: food_2,
+    price: 15000,
+    category: "Burgers",
+  },
+  {
+    _id: "3",
+    name: "Double Cheese Burger",
+    image: food_3,
+    price: 15000,
+    category: "Burgers",
+  },
+  {
+    _id: "4",
+    name: "Spicy Chicken Burger",
+    image: food_4,
+    price: 15000,
+    category: "Burgers",
+  },
+  {
+    _id: "5",
+    name: "Meat Pie",
+    image: food_5,
+    price: 15000,
+    category: "Snacks & Small Chops",
+  },
+  {
+    _id: "6",
+    name: "Sausage Roll",
+    image: food_6,
+    price: 5000,
+    category: "Snacks & Small Chops",
+  },
+  {
+    _id: "7",
+    name: "Puff Puff",
+    image: food_7,
+    price: 5000,
+    category: "Snacks & Small Chops",
+  },
+  {
+    _id: "8",
+    name: "Gizzdodo(gizzard & plantain mix)",
+    image: food_8,
+    price: 10000,
+    category: "Snacks & Small Chops",
+  },
+  {
+    _id: "9",
+    name: "Chicken Shawarma",
+    image: food_9,
+    price: 10000,
+    category: "Shawarma & Wraps",
+  },
+  {
+    _id: "10",
+    name: "Beef Shawarma",
+    image: food_10,
+    price: 10000,
+    category: "Shawarma & Wraps",
+  },
+  {
+    _id: "11",
+    name: "Shawarma with Sausage & Cheese",
+    image: food_11,
+    price: 10000,
+    category: "Shawarma & Wraps",
+  },
+  {
+    _id: "12",
+    name: "Suya Chicken Shawarma",
+    image: food_12,
+    price: 10000,
+    category: "Shawarma & Wraps",
+  },
+  {
+    _id: "13",
+    name: "French Fries",
+    image: food_13,
+    price: 6500,
+    category: "Sides & Extras",
+  },
+  {
+    _id: "14",
+    name: "Moi-Moi",
+    image: food_14,
+    price: 4000,
+    category: "Sides & Extras",
+  },
+  {
+    _id: "15",
+    name: "Fried Plantain",
+    image: food_15,
+    price: 5000,
+    category: "Sides & Extras",
+  },
+  {
+    _id: "16",
+    name: "Coleslaw",
+    image: food_16,
+    price: 3000,
+    category: "Sides & Extras",
+  },
+  {
+    _id: "17",
+    name: "Spicy Fried Chicken",
+    image: food_17,
+    price: 9000,
+    category: "Fried Chicken",
+  },
+  {
+    _id: "18",
+    name: "BBQ Chicken Wings",
+    image: food_18,
+    price: 9000,
+    category: "Fried Chicken",
+  },
+  {
+    _id: "19",
+    name: "Chicken and Chips",
+    image: food_19,
+    price: 9000,
+    category: "Fried Chicken",
+  },
+  {
+    _id: "20",
+    name: "Turkey Drumsticks",
+    image: food_20,
+    price: 9500,
+    category: "Fried Chicken",
+  },
+  {
+    _id: "21",
+    name: "Jollof Rice",
+    image: food_21,
+    price: 10000,
+    category: "Nigerian Meals",
+  },
+  {
+    _id: "22",
+    name: "Fried Rice with Plantain",
+    image: food_22,
+    price: 10000,
+    category: "Nigerian Meals",
+  },
+  {
+    _id: "23",
+    name: "Yam Porridge with Beef",
+    image: food_23,
+    price: 9000,
+    category: "Nigerian Meals",
+  },
+  {
+    _id: "24",
+    name: "Spaghetti Stir-Fry with Turkey",
+    image: food_24,
+    price: 10000,
+    category: "Nigerian Meals",
+  },
+  {
+    _id: "25",
+    name: "Zobo Drink",
+    image: food_25,
+    price: 8500,
+    category: "Drinks & Desserts",
+  },
+  {
+    _id: "26",
+    name: "Chapman",
+    image: food_26,
+    price: 8000,
+    category: "Drinks & Desserts",
+  },
+  {
+    _id: "27",
+    name: "Ice Cream Cup",
+    image: food_27,
+    price: 9000,
+    category: "Drinks & Desserts",
+  },
+  {
+    _id: "28",
+    name: "Bottled Water or Soft Drinks",
+    image: food_28,
+    price: 9000,
+    category: "Drinks & Desserts",
+  },
+];
